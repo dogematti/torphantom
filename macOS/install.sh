ce01778610bcd649b7aaa375d9b88f7e6fe5bbd2
Dogematti <--global>
2024-03-18 18:22:15 +0200

    Fixed for macOS
    Updated MacOS-version to a working one.

--- macOS/install.sh
+++ macOS/install.sh
@@ -1,12 +1,10 @@
 echo "******* TorPhantom Installer ********"
 echo ""
 echo "=====> Installing macchanger "
-git clone https://github.com/InjectionSoftwareDevelopment/macchanger
+git clone https://github.com/shilch/macchanger
 cd macchanger
-sudo chmod +x install.sh
-./install.sh
+echo "alias macchanger='$PWD/macchanger.sh'" >> ~/.zshrc
 cd ../
-rm -rf macchanger
 echo "=====> Installing tor bundle "
 sudo port install tor
 sudo mv /opt/local/etc/tor/torrc.sample /opt/local/etc/tor/torrc
